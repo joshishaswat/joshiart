f09296ece7429cebc24d5e466a5ad52069c2dc14
joshishaswat <joshishaswat@gmail.com>
2024-07-15 13:42:55 -0400

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+	<div>
+		<h1 className="text-3xl font-bold underline">
+			Website under construction
+		</h1>
+		<h1 className="text-blue-500">Check back soon!</h1>
+	</div>
+    )
+}
