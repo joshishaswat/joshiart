16b1d3ef553ccb98f67aafd36000a9f73c1f8e9e
joshishaswat <joshishaswat@gmail.com>
2024-07-21 17:02:50 -0400

    added stripe button functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import "./index.css"
+import {BuyButtonComponent1, BuyButtonComponent2, BuyButtonComponent3} from './stripeButton'
 
 function Header() {
 	return (
@@ -49,7 +50,7 @@ function Body() {
           <a className="text-lg font-semibold leading-tight text-gray-900">Dark Sailboat</a>
 
           <div className="mb-1 flex items-center justify-between gap-4">
-            <p className="text-sm py-0.5 pt-3 text-xs font-semibold">8in X 12in</p>
+            <p className="text-sm py-0.5 pt-3 text-xs font-semibold">16in X 12in</p>
           </div>
 
           <div className="mb-12 flex items-center justify-between gap-4">
@@ -59,12 +60,7 @@ function Body() {
           <div className="mt-4 flex items-center justify-between gap-4">
             <p className="text-l font-extrabold leading-tight text-gray-900">$45.00</p>
 
-            <button type="button" className="inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-small font-normal text-gray-950 hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300">
-              <svg className="-ms-2 me-2 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6" />
-              </svg>
-              Buy
-            </button>
+            <BuyButtonComponent1 />
           </div>
         </div>
       </div>
@@ -89,12 +85,7 @@ function Body() {
           <div className="mt-4 flex items-center justify-between gap-4">
             <p className="text-l font-extrabold leading-tight text-gray-900">$35.00</p>
 
-            <button type="button" className="inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-small font-normal text-gray-950 hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300">
-              <svg className="-ms-2 me-2 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6" />
-              </svg>
-              Buy
-            </button>
+            <BuyButtonComponent2 />
           </div>
         </div>
       </div>
@@ -120,12 +111,7 @@ function Body() {
           <div className="mt-4 flex items-center justify-between gap-4">
             <p className="text-l font-extrabold leading-tight text-gray-900">$30.00</p>
 
-            <button type="button" className="inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-small font-normal text-gray-950 hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300">
-              <svg className="-ms-2 me-2 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6" />
-              </svg>
-              Buy
-            </button>
+            <BuyButtonComponent3 />
           </div>
         </div>
       </div>
@@ -150,12 +136,6 @@ function Body() {
           <div className="mt-4 flex items-center justify-between gap-4">
             <p className="text-l font-extrabold leading-tight text-gray-900">SOLD</p>
 
-            <button type="button" className="inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-small font-normal text-gray-950 hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300">
-              <svg className="-ms-2 me-2 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6" />
-              </svg>
-              Buy
-            </button>
           </div>
         </div>
       </div>
@@ -190,15 +170,21 @@ function Footer() {
 export default function Home() {
   return (
 	<div>
-	<div>
-		<Header />
-	</div>
-	<div>
-		<Body />
-	</div>
-	<div>
-		<Footer />
-	</div>
+    <head>
+      <script
+      async 
+      src="https://js.stripe.com/v3/buy-button.js">
+      </script>
+    </head >
+    <div>
+      <Header />
+    </div>
+    <div>
+      <Body />
+    </div>
+    <div>
+      <Footer />
+    </div>
 	</div>
     )
 }
---
+++ app/stripeButton.js
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+function BuyButtonComponent1() {
+  return (
+    <stripe-buy-button
+      buy-button-id="buy_btn_1Pf6R1BRsR6qCZYFsaKWQ7x8"
+      publishable-key="pk_live_51Hqob6BRsR6qCZYFwfbhEu4qHawcJx9MJBArUnND0Md4a84sQbJCAVLPMcyvdeaU5PT64m9CUPlgB49lEDs5rIzd00NUCYbm3Y"
+    >
+    </stripe-buy-button>
+  );
+}
+
+function BuyButtonComponent2() {
+  return (
+    <stripe-buy-button
+      buy-button-id="buy_btn_1Pf6TSBRsR6qCZYF8YEpBC5m"
+      publishable-key="pk_live_51Hqob6BRsR6qCZYFwfbhEu4qHawcJx9MJBArUnND0Md4a84sQbJCAVLPMcyvdeaU5PT64m9CUPlgB49lEDs5rIzd00NUCYbm3Y"
+    >
+    </stripe-buy-button>
+  );
+}
+
+function BuyButtonComponent3() {
+  return (
+    <stripe-buy-button
+      buy-button-id="buy_btn_1Pf6TvBRsR6qCZYFqhkmvb9r"
+      publishable-key="pk_live_51Hqob6BRsR6qCZYFwfbhEu4qHawcJx9MJBArUnND0Md4a84sQbJCAVLPMcyvdeaU5PT64m9CUPlgB49lEDs5rIzd00NUCYbm3Y"
+    >
+    </stripe-buy-button>
+  );
+}
+
+export {
+  BuyButtonComponent1,
+  BuyButtonComponent2,
+  BuyButtonComponent3
+}
