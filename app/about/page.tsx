f624f12375938a759a199ed8e9f0550c6dfe845d
joshishaswat <joshishaswat@gmail.com>
2024-08-01 20:32:17 -0400

    changed color scheme in about me section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,13 +5,13 @@ import Footer from '../footer'
 function AboutMe() {
     return (
         <div>
-        <section className="bg-slate-900">
+        <section className="bg-sky-950">
             <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                 <div className="mr-auto place-self-center lg:col-span-7">
                     {/* <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-slate-100">Welcome To My Art World</h1> */}
-                    <h3 className="inline-flex gap-3 max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-xl lg:text-2xl dark:text-gray-400 ">
+                    <h3 className="inline-flex gap-3 max-w-2xl mb-6 font-light text-gray-50 lg:mb-8 md:text-xl lg:text-2xl">
                         Chapel Hill, NC 
-                        <svg width="30" height="20" viewBox="0 0 30 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg transform="translate(10,4)" width="30" height="20" viewBox="0 0 30 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M30 0H0V20H30V0Z" fill="#F0F0F0"/>
                         <path d="M30 2.49976H0V4.99951H30V2.49976Z" fill="#D80027"/>
                         <path d="M30 7.49994H0V9.9997H30V7.49994Z" fill="#D80027"/>
@@ -32,12 +32,13 @@ function AboutMe() {
                         <path d="M12.4526 1.72961L12.211 2.47287H11.4297L12.0619 2.932L11.8203 3.67514L12.4526 3.216L13.0846 3.67514L12.843 2.932L13.4753 2.47287H12.6938L12.4526 1.72961Z" fill="#F0F0F0"/>
                         </svg>
                     </h3>
-                    <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">My work revolves around impressionistic, contemporary and figurative art.</p>
-                    <a href="/" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                    <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-zinc-300">My interest in art began when I visited the National Gallery of Art in Washington DC. I was inspired by the calming effect I had when looking at the art and aspire to recreate that effect. I studied Political Science at UNC Chapel Hill and work as a Software Engineer. For my next project I would like to do a portrait painting.</p>
+                    <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-zinc-300">For inquiries email me at joshishaswat@gmail.com</p>
+                    <a href="/" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-sky-800">
                         Return to Store
                         <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                     </a>
-                    <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                    <a href="#sectionId" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-sky-800">
                         See Sold Works
                         <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                     </a>
@@ -48,10 +49,7 @@ function AboutMe() {
             </div>
         </section>
 
-
-
-        
-        <section className="bg-white py-8 antialiased md:py-12">
+        <section id="sectionId" className="bg-white py-8 antialiased md:py-12">
             <div>
                 <h2 className="text-stone-950 text-l font-semibold text-center mb-8">Sold Works</h2>
             </div>
@@ -87,10 +85,6 @@ function AboutMe() {
         </section>
     </div>
     )
-}
-
-function SoldItems() {
-
 }
 
 export default function About() {
